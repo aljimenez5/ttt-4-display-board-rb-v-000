6e1edeca0e67597c142750252744e0780b05889d
arvida.jimenez@gmail.com <arvida.jimenez@gmail.com>
2018-07-22 02:15:08 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,12 +1,5 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 board = ["  ","  ","  ","  ","  ","  ","  ","  ","  "]
-def display_board
-    a = "   |   |   "
-    b = "-----------"
-    puts a
-    puts b
-    puts a
-    puts b
-    puts a
-	end
+def display_board(board)
+  
