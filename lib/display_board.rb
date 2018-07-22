03b6d3f1285ade00c2e3fc01abc0e163f214b6a4
arvida.jimenez@gmail.com <arvida.jimenez@gmail.com>
2018-07-22 02:10:50 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,11 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board
+    a = "   |   |   "
+    b = "-----------"
+    puts a
+    puts b
+    puts a
+    puts b
+    puts a
+	end
