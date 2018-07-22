6b9efbed478f302a341c88faad533d149c349c4e
arvida.jimenez@gmail.com <arvida.jimenez@gmail.com>
2018-07-22 02:19:15 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,4 +2,13 @@
 # out the current state.
 board = ["  ","  ","  ","  ","  ","  ","  ","  ","  "]
 def display_board(board)
-  
+
+
+      a = " #{board[0]}  | #{board[1]}  | #{board[2]}  "
+      b = "-----------"
+      puts a
+      puts b
+      puts a
+      puts b
+      puts a
+  	end
